b5cbce1ad98decf3fac423d910b6ac7e4b7984b5
orom915 <73530985+orom915@users.noreply.github.com>
2020-11-05 13:03:25 +0100

    Create indexa.blade.php

---
+++ indexa.blade.php
@@ -0,0 +1,45 @@
+@extends("theme.$theme.layout")
+@section('encabezado')
+    @include("admin/admin/headeradmin")
+@endsection
+@section('contenido')
+    @include('mensajes.exito')
+    <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Usuarios</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <table class="table table-bordered">
+            <tbody><tr>
+              <th style="width: 10px">Id</th>
+              <th>Email</th>
+              <th style="width: 20px"></th>
+              <th style="width: 20px"></th>
+            </tr>
+            @foreach ($usuarios as $usuario)
+            <tr>
+              <td>
+                {{$usuario->id}}
+              </td>
+              <td>
+                {{$usuario->email}}
+              </td>
+              <td>
+                <a href="{{route('usuario_editar', ['id' => $usuario->id])}}" class="btn-accion-tabla tooltipsC" title="Editar usuario">
+                    <i class="fa fa-edit"></i>
+                </a></td><td>
+                <form action="{{route('usuario_eliminar', ['id' => $usuario->id])}}" class="d-inline form-eliminar" method="POST">
+                    @csrf @method("delete")
+                    <button type="submit" class="btn-accion-tabla eliminar tooltipsC" title="Eliminar usuario">
+                        <i class="fa fa-times-circle text-danger"></i>
+                    </button></td>
+                </form>
+            </td>
+            </tr>
+        @endforeach
+            </tbody>
+          </table>
+        </div>
+    </div>
+@endsection
