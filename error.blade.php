ea5dec9aeeda4f9125960190170082d0a3c0b64e
orom915 <73530985+orom915@users.noreply.github.com>
2020-11-05 12:50:08 +0100

    Create error.blade.php

---
+++ error.blade.php
@@ -0,0 +1,11 @@
+@if (session('mensaje'))
+<div class="alert alert-danger alert-dismissible" >
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <h4><i class="icon fa fa-ban"></i> Alerta</h4>
+    <ul>
+            <li>
+                {{session('mensaje')}}
+            </li>
+    </ul>
+  </div>
+@endif
