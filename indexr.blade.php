c5f8224f6bd3feae269f055405fb868e1ba1f67a
orom915 <73530985+orom915@users.noreply.github.com>
2020-11-25 13:23:51 +0100

    Update indexr.blade.php

--- indexr.blade.php
+++ indexr.blade.php
@@ -2,21 +2,22 @@
 @section('encabezado')
     @include("admin/admin/headeradmin")
 @endsection
+@section('lateral')
+    @include("admin/admin/aside")
+@endsection
 @section('contenido')
-<div class="row">
-    <div class="col-lg-12">
-        @include('mensajes.exito')
-        <div class="card card-info">
-            <div class="card-header">
-                <h3 class="card-title">Rol</h3>
-                <div class="card-tools">
-                    <a href="{{route('rol_crear')}}" class="btn btn-outline-secondary btn-sm">
-                        <i class="fa fa-fw fa-plus-circle"></i> Nuevo registro
-                    </a>
-                </div>
-            </div>
-            <div class="card-body table-responsive p-0">
-                <table class="table table-striped table-bordered table-hover" id="tabla-data">
+    @include('mensajes.exito')
+<div class="box box-info">
+    <div class="box-header with-border">
+        <h3 class="box-title"><b>Rol</b></h3>
+    </div>
+        <div class="card-tools">
+            <a href="{{route('rol_crear')}}" class="btn btn-outline-secondary btn-sm">
+                 <i class="fa fa-fw fa-plus-circle"></i> Nuevo registro
+            </a>
+        </div>
+            <div class="box-body">
+                <table class="table table-bordered" id="tabla-data">
                     <thead>
                         <tr>
                             <th class="width20">ID</th>
@@ -29,23 +30,23 @@
                         <tr>
                             <td>{{$data->id}}</td>
                             <td>{{$data->nombre}}</td>
-                            <td>
+                            <td style="width: 20px">
                                 <a href="{{route('rol_editar', ['id' => $data->id])}}" class="btn-accion-tabla tooltipsC" title="Editar este registro">
                                     <i class="fa fa-edit"></i>
                                 </a>
+                                <td style="width: 20px">
                                 <form action="{{route('rol_eliminar', ['id' => $data->id])}}" class="d-inline form-eliminar" method="POST">
                                     @csrf @method("delete")
                                     <button type="submit" class="btn-accion-tabla eliminar tooltipsC" title="Eliminar este registro">
                                         <i class="fa fa-times-circle text-danger"></i>
                                     </button>
                                 </form>
+                                </td>
                             </td>
                         </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
-        </div>
-    </div>
 </div>
 @endsection
