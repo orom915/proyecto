67b17f678349725df18643222602900cfee1eef0
orom915 <73530985+orom915@users.noreply.github.com>
2020-12-07 12:33:48 +0100

    Create test.blade.php

---
+++ test.blade.php
@@ -0,0 +1,20 @@
+<div class="box box-danger">
+    <div class="box-header with-border">
+      <h3 class="box-title"><b>Archivo de tests</b></h3>
+    </div>
+    <div class="box-body">
+      <table class="table table-bordered">
+        <tbody>
+            <tr>
+                <th style="width: 200px">Archivo</th>
+                <th style="width: 20px">Descargar</th>
+                <th style="width: 20px">Recargar</th>
+            </tr>
+            <tr>
+                <th>Archivo de tests actual</th>
+                <th><a href="{{route('descargar_test')}}" class="glyphicon glyphicon-download-alt"></a></th>
+                <th><a href="{{route('importar_test')}}" class="glyphicon glyphicon-refresh"></a></th>
+        </tbody>
+      </table>
+    </div>
+</div>
