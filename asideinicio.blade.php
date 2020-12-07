c8ecef34cb97f3952fb867d9eacc03c83bfa0dfb
orom915 <73530985+orom915@users.noreply.github.com>
2020-12-07 12:39:07 +0100

    Create asideinicio.blade.php

---
+++ asideinicio.blade.php
@@ -0,0 +1,13 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+      <div class="user-panel">
+      </div>
+      <ul class="sidebar-menu" data-widget="tree">
+        <li>
+          <a href="{{route('login')}}">
+            <i class="glyphicon glyphicon-log-in"></i> <span>Iniciar Sesion</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </aside>
