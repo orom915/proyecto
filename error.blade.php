1aea87cef4dcc3e53a60ad837dfcbecdd030b677
orom915 <73530985+orom915@users.noreply.github.com>
2020-12-17 11:32:47 +0100

    Update error.blade.php

--- error.blade.php
+++ error.blade.php
@@ -1,6 +1,6 @@
 @if (session('mensaje'))
-<div class="alert alert-danger alert-dismissible" >
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+<div class="alert alert-danger alert-dismissible" auto-close="5000">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true" >×</button>
     <h4><i class="icon fa fa-ban"></i> Alerta</h4>
     <ul>
             <li>
