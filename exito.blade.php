9eab6113ef291ac66e61aafe2c9fb6eb414e2945
orom915 <73530985+orom915@users.noreply.github.com>
2020-12-17 11:32:20 +0100

    Update exito.blade.php

--- exito.blade.php
+++ exito.blade.php
@@ -1,5 +1,5 @@
 @if (session('mensaje'))
-<div class="alert alert-success alert-dismissible">
+<div class="alert alert-success alert-dismissible" auto-close="5000">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <h4><i class="icon fa fa-check"></i> Correcto</h>
     <ul>
